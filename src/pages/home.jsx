1cc65ea75e02428fa740bf2d34b21af2c45cfaf9
Giohuram <130119153+Giohuram@users.noreply.github.com>
2023-12-07 15:41:51 +0000

    added avatar class name

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,37 +8,36 @@ function Home() {
         <img src="src/images/Icons/Timeline-Prop.png" alt="star-icon"/>
       </div>
       <div className='tweet-area'>
-        <div className='profile-pik'>
-          <img src="" />
+        <div className='avatar'>
+          <img src="src/images/profile-photo.png" alt="profilepik"/>
         </div>
         <div className='tweet-editor'>
           <textarea className='tweet-editor-form tweet-editor-input'placeholder="what's happening here?"></textarea>
           <div className="tweet-editor-buttons">
-          <img 
-            src="src/images/Icons/Media.png"
-            alt="Media"  
-          />
-          <img 
-            src="src/images/Icons/Gif.png"
-            alt="Gif"  
-          />
-          <img 
-            src="src/images/Icons/Poll.png"
-            alt="Poll-icon"  
-          />
-          <img 
-            src="src/images/Icons/Emoji.png"
-            alt="Emoji"  
-          />
-          <img 
-            src="src/images/Icons/Schedule.png"
-            alt="Schedule"  
-          />
-          <div className='tweet-editor-actions button'>
-            <button className='button'>Tweet</button>
+            <img 
+              src="src/images/Icons/Media.png"
+              alt="Media"  
+            />
+            <img 
+              src="src/images/Icons/Gif.png"
+              alt="Gif"  
+            />
+            <img 
+              src="src/images/Icons/Poll.png"
+              alt="Poll-icon"  
+            />
+            <img 
+              src="src/images/Icons/Emoji.png"
+              alt="Emoji"  
+            />
+            <img 
+              src="src/images/Icons/Schedule.png"
+              alt="Schedule"  
+            />
+            <div className='tweet-editor-actions button'>
+              <button className='button'>Tweet</button>
+            </div>
           </div>
-        </div>
-          
         </div>
       </div>
       <div className='Tweets'>
